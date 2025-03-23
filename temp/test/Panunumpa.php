d7cd7037c536fc73c0de7e180c5bdfa2866b0d10
Unknown-user11 <kennethtaduran003@gmail.com>
2025-03-23 14:18:39 +0800

    Add files via upload

---
+++ temp/test/Panunumpa.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="panunumpa.css">
+
+</head>
+<body>
+    
+<div class="container">
+    <nav>
+        <ul>
+            <li>
+                <a href="#">About Us</a>
+                <ul class="dropdown">
+                    <li><a href="VM.php">Vision, Mission and Core Values</a></li>
+                    <li><a href="PNA.php">Philippine National Anthem</a></li>
+                    <li><a href="Panunumpa.php">Panunumpa sa watawat ng Pilipinas</a></li>
+                </ul>
+            </li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Admissions</a></li>
+            <li><a href="#">Enrollment</a></li>
+            <li><a href="#">Contact Us</a></li>
+            </li>
+        </ul>
+        </div>
+    </nav>
+</div>
+<img src="Kalayaan.png" class="logo">
+<div id="textlogo">
+<div id="agency-tagline">School Division Office - Caloocan city</div>
+<div id="agency-name">Kalayaan National High School</div><br>
+</div>
+
+<div class="space"></div>
+<div id="element-paragraph">
+<p class="element-p">UNDER MAINTENANCE</p>
+</div>
+
+<div class="space-1"></div>
+
+<div id="element-paragraph1">
+    <p class="element-i"></p>
+</div>
+
+<footer>
+    <p>&copy; Kalayaan National High School. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
---
+++ temp/test/VM.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="VM.css">
+
+</head>
+<body>
+    
+<div class="container">
+    <nav>
+        <ul>
+            <li>
+                <a href="#">About Us</a>
+                <ul class="dropdown">
+                    <li><a href="VM.php">Vision, Mission and Core Values</a></li>
+                    <li><a href="PNA.php">Philippine National Anthem</a></li>
+                    <li><a href="Panunumpa.php">Panunumpa sa watawat ng Pilipinas</a></li>
+                </ul>
+            </li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Admissions</a></li>
+            <li><a href="#">Enrollment</a></li>
+            <li><a href="#">Contact Us</a></li>
+            </li>
+        </ul>
+        </div>
+    </nav>
+</div>
+<img src="Kalayaan.png" class="logo">
+<div id="textlogo">
+<div id="agency-tagline">School Division Office - Caloocan city</div>
+<div id="agency-name">Kalayaan National High School</div><br>
+</div>
+
+<div class="space"></div>
+
+
+<div class="element-column">
+    <div id="column-1">VISION</div>
+    <div id="column-2">MISSION</div>
+</div>
+
+
+<div class="element-container">
+    <div class="column-1"> Our Vision: 
+        <p><br>To produce graduates who are productive and <br> caring citizens 
+        who are prepared to succeed in a <br> global society.</p></div>
+
+    <div class="column-2">Our mission:
+<p><br>The KNHS community:</p>
+<span class="element-span">D</span>esires excellence<br>
+<span class="element-span">R</span>espect others<br>
+<span class="element-span">E</span>ngages meaningful work<br>
+<span class="element-span">A</span>ccepts everyone<br>
+<span class="element-span">M</span>otivates and inspires everyone everyday<br>
+<span class="element-span">S</span>upports all in a safe and enriching learning environment<br>
+</div>
+
+</div>
+
+<footer>
+    <p>&copy; Kalayaan National High School. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
